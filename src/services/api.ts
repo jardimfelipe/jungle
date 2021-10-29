1e98d294641420292884266d9c71ff532bcccc3a
Jonathan Candido <jhlcandido@gmail.com>
2021-10-29 14:56:01 -0300

    github actions

--- src/services/api.ts
+++ src/services/api.ts
@@ -3,28 +3,35 @@ import store from "../store";
 import { logout } from "../store/modules/login/actions";
 import { getSavedState, removeState } from "../utils/localStorage";
 
-const baseURL = window.location.href === 'http://homologacao.app.junglexp.com.br/' ? 'https://jungle-api-hml-ozjujigmba-uc.a.run.app/' : process.env.REACT_APP_API_BASE_URL
+const baseURL =
+  window.location.href.indexOf("hml.app") >= 0 ||
+  window.location.href.indexOf("localhost") >= 0
+    ? "https://jungle-api-hml-ozjujigmba-uc.a.run.app/"
+    : process.env.REACT_APP_API_BASE_URL;
 
 const api = axios.create({
   baseURL,
 });
 
 api.interceptors.request.use(function (config) {
-  const token = getSavedState('auth.token')
-  if (token) config.headers.Authorization = `Bearer ${token}`
-  return config
-})
-
-api.interceptors.response.use(function (response) {
-  return response;
-}, function (error) {
-  if (error.response.status === 401) {
-    alert('Token expirado')
-    removeState('auth.token')
-    store.dispatch(logout())
-    document.location.reload()
-  }
-  return Promise.reject(error);
+  const token = getSavedState("auth.token");
+  if (token) config.headers.Authorization = `Bearer ${token}`;
+  return config;
 });
+
+api.interceptors.response.use(
+  function (response) {
+    return response;
+  },
+  function (error) {
+    if (error.response.status === 401) {
+      alert("Token expirado");
+      removeState("auth.token");
+      store.dispatch(logout());
+      document.location.reload();
+    }
+    return Promise.reject(error);
+  }
+);
 
 export default api;
